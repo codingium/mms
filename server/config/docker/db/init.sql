40d31c0f9f171c4ae0a69ece70342c56e64ba4f5
TorokDan <dani.torok8@gmail.com>
2022-12-20 13:58:00 +0100

    project(): psql, flyway basic setup

--- server/config/docker/db/init.sql
+++ server/config/docker/db/init.sql
@@ -1,3 +1,5 @@
 create database mms;
 create user mms with encrypted password 'mms';
 grant all privileges on database mms to mms;
+\c mms postgres
+grant all on schema public to mms;
--- server/config/docker/db/start.sh
+++ server/config/docker/db/start.sh
@@ -16,6 +16,9 @@ elif [ "$1" == "delete" ]; then
   docker rm $name > /dev/null
   echo "delete image: ${tag}:latest"
   docker rmi "${tag}:latest" > /dev/null
+elif [ "$1" == "start" ]; then
+  echo "start container: ${name}"
+  docker run --name $name -p 5432:5432 -d "${tag}:latest" > /dev/null
 else
   echo "use create or delete on command"
 fi
