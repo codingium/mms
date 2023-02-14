40ae934d010f1e28fe67b3ca8cb0c410159abd59
JakusDani <55551015+JakusDani@users.noreply.github.com>
2023-02-14 21:03:22 +0100

    DB - Felvettem a táblákat, nem teszteltem

--- server/src/main/resources/db/migration/V1_0__create_tables.sql
+++ server/src/main/resources/db/migration/V1_0__create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Role(
+CREATE TABLE RoleType(
     ID INT GENERATED ALWAYS AS IDENTITY,
     Name VARCHAR(30),
     Remark VARCHAR(200),
@@ -22,6 +22,69 @@ CREATE TABLE Users (
          FOREIGN KEY(RoleTypeID)
              REFERENCES Role(ID)
 );
+
+CREATE TABLE Options (
+    ID INT GENERATED ALWAYS AS IDENTITY,
+    Key VARCHAR(50),
+    Value VARCHAR(50),
+    Remark VARCHAR(200),
+    CreateDateTime TIMESTAMP DEFAULT NOW(),
+    LastModifiedDateTime TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (ID)
+)
+
+CREATE TABLE CurrencyType(
+    ID INT GENERATED ALWAYS AS IDENTITY,
+    FullName VARCHAR(50),
+    ShortName VARCHAR(50),
+    Symbol CHAR,
+    Remark VARCHAR(200),
+    CreateTime TIMESTAMP DEFAULT NOW(),
+    LastModifiedTime TIMESTAMP DEFAULT NOW()
+)
+
+CREATE TABLE Invoices (
+    ID INT GENERATED ALWAYS AS IDENTITY,
+    UserID INT,
+    --CurrencyID INT, <-- Ez is minek van??
+    AllMoney INT,
+    MoneyRemain INT,
+    --StatusID PK INT, <-- na oszt ez mi a ****!?
+    Remark VARCHAR(200),
+    CreateDateTime TIMESTAMP DEFAULT NOW(),
+    LastModifiedDateTime TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY(ID),
+        CONSTRAINT  fk_User
+             FOREIGN KEY(UserID)
+                 REFERENCES Users(ID),
+)
+
+CREATE TABLE CategoryType(
+    ID INT GENERATED ALWAYS AS IDENTITY,
+    Name VARCHAR(50),
+    Remark VARCHAR(200),
+    CreateTime TIMESTAMP DEFAULT NOW(),
+    LastModifiedTime TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY(ID)
+)
+
+CREATE TABLE MoneyHistory(
+    ID INT GENERATED ALWAYS AS IDENTITY,
+    --ManagmentID INT, <-- Ez is mi a rákk!?
+    CategoryTypeID INT,
+    CurrencyID INT,
+    MoneySpent INT,
+    Remark VARCHAR(200),
+    CreateTime TIMESTAMP DEFAULT NOW(),
+    LastModifiedTime TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY(ID),
+    CONSTRAINT fk_Category
+        FOREIGN KEY(CategoryTypeID)
+        REFERENCES CategoryType(ID)
+    CONSTRAINT fk_Curenncy
+        FOREIGN KEY(CurrnecyID)
+        REFERENCES CurrencyType(ID)
+)
 
 -- INSERT INTO Role(Name) VALUES('Test');
 -- INSERT INTO Users(RoletypeID, FirstName, LastName) VALUES
